c6e96526e5b34db61d272b2d8728e99bd7e3d284
207kioko <107875372+207kioko@users.noreply.github.com>
2024-04-02 11:55:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from datetime import datetime
-from keras.models import load_model
 import yfinance as yf
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
